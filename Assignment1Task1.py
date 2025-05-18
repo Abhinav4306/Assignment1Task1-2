472f53a936d43d2c63c4c8d53c885bff850e8fcd
Abhinav4306 <abhinav6804@gmail.com>
2025-05-18 10:55:11 +0530

    Add files via upload

---
+++ Assignment1Task1.py
@@ -0,0 +1,12 @@
+a=input("Enter the First Number : ")
+b=input("Enter the Second Number : ")
+a=float(a)
+b=float(b)
+c=a+b
+print("Addition of these two numbers is: ",c)
+d=a-b
+print("Subtraction of these two numbers is: ",d)
+e=a*b
+print("Multiplication of these two numbers is: ",e)
+f=a/b
+print("Division of these two numbers is: ",f)
